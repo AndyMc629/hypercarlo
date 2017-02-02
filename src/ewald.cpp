987858bc828b7fc6754a310244d729ca4509761c
APM13 <andrewpmcmahon629@gmail.com>
2017-02-02 20:59:43 +0000

    adding ewald.cpp file, going to put ewald summation function(s) in here

---
+++ src/ewald.cpp
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+ * 
+ * 
+ * SEE "Computer simulations of dipolar fluids using ewald summations" in my 
+ * Ewald SUmmation Mendeley folder and compare with ewald.cpp in "EwaldSummation"
+ * folder on GitHub (pretty certain they are the same so can use this! I think...,
+ * be careful to make sure that one is for dipoles and not for charges!
+ *  */
+
